c9a45de3001d2c64eda3a4738e52aee9de23cf33
David Owen <david.owen@csuglobal.edu>
2024-07-20 09:27:17 -0500

    Week 1 end

--- CPPPractice/MathCPP.cpp
+++ CPPPractice/MathCPP.cpp
@@ -10,6 +10,25 @@ int main() {
 
     students+=1; // different way to write the math, just like java
 
+    // students = students - 1;
+    // students -= 2;
+    // students--;
+
+    // students * 2;
+    // students *= 2;
+
+    // students /= 3; leaves the most you can have, since 20, there will be 6, being 18 is the highest
+    // we can go
+
+    // using the % and you can get the remainder, so 
+    // students % 2; this will actually tell you if it is odd or even
+    // if you do
+    // students % 3; 
+    // the answer will be the remainder of 2
+
+    // 
+
+
     std::cout << students << std::endl; // print out the new variable. 
 
 
